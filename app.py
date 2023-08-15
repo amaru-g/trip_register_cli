7866c0728fe0f96dcbc6f439a0aa9ddb80bd6ed6
Amaru <amaru@pop-os.localdomain>
2023-08-15 23:43:47 +0200

    asdf

---
+++ app.py
@@ -0,0 +1,9 @@
+class Trip:
+    def __init__(self, booking_code, comments_about_booking):
+        self.booking_code = booking_code
+        self.comments_about_booking = comments_about_booking
+
+class node(self,name,entry_date,direction):
+    pass
+class expense(self,type,currency,ammount,comments):
+    pass
